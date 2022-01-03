3adc323f64337ff11ea8c451e34c9b5e0a6fdabc
Willian Menezes dos Santos <willian.santos@bs2tecnologia.com.br>
2022-01-03 10:12:17 -0300

    Adicionado funcao recusiva para contagem regressiva

--- Algoritmos/FuncoesRecursivas.cs
+++ Algoritmos/FuncoesRecursivas.cs
@@ -7,6 +7,11 @@
             var numeroParaSerCalculado = int.Parse(Console.ReadLine() ?? "0");
 
             Console.WriteLine($"Fatorial de {numeroParaSerCalculado} eh:  {Fatorial(numeroParaSerCalculado)}");
+
+            Console.WriteLine();
+            Console.WriteLine("Iniciando a funcao de contagem regressiva.");
+
+            ContagemRegressiva(10);
         }
 
         private static int Fatorial(int x)
@@ -14,6 +19,17 @@
             if (x == 1) return 1;
 
             return x * Fatorial(x - 1);
+        }
+
+        private static void ContagemRegressiva(int valor)
+        {
+            Console.WriteLine(valor);
+
+            if (valor <= 0)
+                return;
+
+            Thread.Sleep(1000);
+            ContagemRegressiva(valor - 1);
         }
     }
 }
