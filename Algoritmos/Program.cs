24623305460f906701cecea3ffd5e96c43c2ac65
Willian Menezes dos Santos <willian.santos@bs2tecnologia.com.br>
2022-01-24 18:20:08 -0300

    Implementando "Pesquisa em largura"

--- Algoritmos/Program.cs
+++ Algoritmos/Program.cs
@@ -2,4 +2,11 @@
 
 //PesquisaBinaria.Executar();
 
-FuncoesRecursivas.Executar();
+//FuncoesRecursivas.Executar();
+
+var resultado = PesquisaEmLargura.EncontrarVendedor();
+
+if (resultado != null)
+    Console.WriteLine($"A pessoa {resultado.Nome} eh um vendedor");
+else
+    Console.WriteLine("Nao existem vendedores");
