81290f2f8834e758bcccc8623035f362e7488c4c
Willian Menezes <willian_menezes_santos@hotmail.com>
2022-01-24 23:09:22 -0300

    Algoritmo de Dijkstra, calculando o caminho minimo para um grafo ponderado.

--- Algoritmos/Program.cs
+++ Algoritmos/Program.cs
@@ -4,9 +4,11 @@
 
 //FuncoesRecursivas.Executar();
 
-var resultado = PesquisaEmLargura.EncontrarVendedor();
+//var resultado = PesquisaEmLargura.EncontrarVendedor();
 
-if (resultado != null)
-    Console.WriteLine($"A pessoa {resultado.Nome} eh um vendedor");
-else
-    Console.WriteLine("Nao existem vendedores");
+//if (resultado != null)
+//    Console.WriteLine($"A pessoa {resultado.Nome} eh um vendedor");
+//else
+//    Console.WriteLine("Nao existem vendedores");
+
+Dijkstra.Processar();
